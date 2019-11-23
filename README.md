e720f488dbe70d8bcbba6e5b4655ae4c1a07d736
katai5plate <katai5plate.sub@gmail.com>
2019-11-23 18:39:30 +0900

    change repo name

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# os-viewer-ra
-create-react-app + react-app-rewired + react-admin + typescript + powershell でなんかいろいろ
+# exp-rewired-ra
+
+create-react-app + react-app-rewired + react-admin + typescript でなんかいろいろ
+
+## やること
+
+- react-app-rewired で設定をいじる
+- material-ui をいじる
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import jsonServerProvider from "ra-data-json-server";
-import fakeDataProvider from "ra-data-fakerest";
-
+// import fakeDataProvider from "ra-data-fakerest";
 import { Admin, Resource, ListGuesser } from "react-admin";
 
+import "./analyzer";
+
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
+// const dataProvider = fakeDataProvider({});
 
 const App: React.FC = () => (
   <Admin dataProvider={dataProvider}>
